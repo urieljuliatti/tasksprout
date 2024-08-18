94679e7b61cea908da58a9bb249c930a946e14ba
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-08-18 13:28:24 -0300

    Improving the Tasks API

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,54 +1,62 @@
 # frozen_string_literal: true
 
-class Api::V1::TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+module Api
+  module V1
+    class TasksController < ApplicationController
+      before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-  # GET /tasks
-  def index
-    @tasks = Task.all
+      # GET /tasks
+      def index
+        @tasks = Task.all
 
-    render json: @tasks
-  end
+        render json: @tasks
+      end
 
-  # GET /tasks/:id
-  def show
-    render json: @task
-  end
+      # GET /tasks/:id
+      def show
+        render json: @task
+      end
 
-  # POST /tasks
-  def create
-    @task = Task.new(task_params)
-    @task.user = current_user # Assumindo que você tem autenticação implementada
+      def edit
+        @task = Task.find(params[:id])
+      end
 
-    if @task.save
-      render json: @task, status: :created, location: @task
-    else
-      render json: @task.errors, status: :unprocessable_entity
+      # POST /tasks
+      def create
+        @task = Task.new(task_params)
+        @task.user = current_user # Assumindo que você tem autenticação implementada
+
+        if @task.save
+          render json: @task, status: :created, location: @task
+        else
+          render json: @task.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /tasks/:id
+      def update
+        if @task.update(task_params)
+          render json: @task
+        else
+          render json: @task.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /tasks/:id
+      def destroy
+        @task.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_task
+        @task = Task.find(params[:id])
+      end
+
+      def task_params
+        params.require(:task).permit(:title, :description, :status, :priority, :due_date)
+      end
     end
-  end
-
-  # PATCH/PUT /tasks/:id
-  def update
-    if @task.update(task_params)
-      render json: @task
-    else
-      render json: @task.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /tasks/:id
-  def destroy
-    @task.destroy
-    head :no_content
-  end
-
-  private
-
-  def set_task
-    @task = Task.find(params[:id])
-  end
-
-  def task_params
-    params.require(:task).permit(:title, :description, :status, :priority, :due_date)
   end
 end
