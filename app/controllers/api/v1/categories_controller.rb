4ccd4caae2fc35871e4ec34f8b629d35d58bfa35
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-08-19 08:50:06 -0300

    adding category controller

---
+++ app/controllers/api/v1/categories_controller.rb
@@ -0,0 +1,56 @@
+
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class CategoriesController < ApplicationController
+      before_action :set_category, only: [:show, :update, :destroy]
+
+
+      def index
+        @categories = Category.all
+        render json: @categories
+      end
+
+
+      def show
+        render json: @category
+      end
+
+
+      def create
+        @category = Category.new(category_params)
+        if @category.save
+          render json: @category, status: :created
+        else
+          render json: @category.errors, status: :unprocessable_entity
+        end
+      end
+
+
+      def update
+        if @category.update(category_params)
+          render json: @category
+        else
+          render json: @category.errors, status: :unprocessable_entity
+        end
+      end
+
+
+      def destroy
+        @category.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_category
+        @category = Category.find(params[:id])
+      end
+
+      def category_params
+        params.require(:category).permit(:name)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
         resources :comments, only: [:index, :create]
       end
       resources :comments
+      resources :categories
     end
   end
 end
